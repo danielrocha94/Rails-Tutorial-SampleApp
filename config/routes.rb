83b4fd0e69da39d3bc7d751d4fbe56c8bdc83e8c
danielrocha94 <danielrocha94@msn.com>
2016-03-02 02:11:19 -0600

    chapter7: Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 	get 'about'   => 'static_pages#about'
 	get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
-
+  resources :users
   #older method: get 'static_pages/help'
 
   # The priority is based upon order of creation: first created -> highest priority.
